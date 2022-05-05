d50b52bb3ca07d398e5d11aa20d8ebd7a1fb638b
tkochem <tiagok989@gmail.com>
2022-05-04 23:24:20 -0300

    editando

--- Site/Site/Repositorio/IContatoRepositorio.cs
+++ Site/Site/Repositorio/IContatoRepositorio.cs
@@ -4,6 +4,9 @@ namespace Site.Repositorio
 {
     public interface IContatoRepositorio
     {
+        ContatoModel ListarPorId(int id);
+        List<ContatoModel> BuscarTodos();
         ContatoModel Adicionar(ContatoModel contato);
+        ContatoModel Atualizar(ContatoModel contato);
     }
 }
